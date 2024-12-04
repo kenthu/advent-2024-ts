8827e75faedf8a67dd4555219aee1e4ed6cdeb29
Kent Hu <kenthu@kenthu.net>
2024-12-04 04:57:20 -0800

    Day 04, part 2

--- src/day04/solution.ts
+++ src/day04/solution.ts
@@ -89,6 +89,70 @@ const findXmas = (filename: string): number => {
   return count;
 };
 
+const findCrossmas = (filename: string): number => {
+  const rows = readLines(filename);
+  const grid = rows.map((row) => row.split(""));
+
+  let count = 0;
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[0].length; col++) {
+      if (grid[row]?.[col] !== "A") {
+        continue;
+      }
+
+      // M.S
+      // .A.
+      // M.S
+      if (
+        grid[row - 1]?.[col - 1] === "M" &&
+        grid[row + 1]?.[col - 1] === "M" &&
+        grid[row - 1]?.[col + 1] === "S" &&
+        grid[row + 1]?.[col + 1] === "S"
+      ) {
+        count++;
+      }
+
+      // S.M
+      // .A.
+      // S.M
+      if (
+        grid[row - 1]?.[col + 1] === "M" &&
+        grid[row + 1]?.[col + 1] === "M" &&
+        grid[row - 1]?.[col - 1] === "S" &&
+        grid[row + 1]?.[col - 1] === "S"
+      ) {
+        count++;
+      }
+
+      // M.M
+      // .A.
+      // S.S
+      if (
+        grid[row - 1]?.[col - 1] === "M" &&
+        grid[row - 1]?.[col + 1] === "M" &&
+        grid[row + 1]?.[col - 1] === "S" &&
+        grid[row + 1]?.[col + 1] === "S"
+      ) {
+        count++;
+      }
+
+      // S.S
+      // .A.
+      // M.M
+      if (
+        grid[row + 1]?.[col - 1] === "M" &&
+        grid[row + 1]?.[col + 1] === "M" &&
+        grid[row - 1]?.[col - 1] === "S" &&
+        grid[row - 1]?.[col + 1] === "S"
+      ) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+};
+
 // Part 1 test
 console.log(findXmas("src/day04/test-input.txt"));
 // 18
@@ -96,3 +160,11 @@ console.log(findXmas("src/day04/test-input.txt"));
 // Part 1
 console.log(findXmas("src/day04/input.txt"));
 // 2551
+
+// Part 2 test
+console.log(findCrossmas("src/day04/test-input.txt"));
+// 9
+
+// Part 2
+console.log(findCrossmas("src/day04/input.txt"));
+// 1985
