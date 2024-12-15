2ed2e5cef24e013f492b0745e007d3f34490dfff
Kent Hu <kenthu@kenthu.net>
2024-12-14 20:28:25 -0800

    Complete day 11, part 2

--- src/day11/solution.ts
+++ src/day11/solution.ts
@@ -1,38 +1,50 @@
 import { readLines } from "lib/input";
+import { countBy, sum } from "lodash";
 
-const processInput = (filename: string): number[] => {
-  return readLines(filename)[0]
-    .split(" ")
-    .map((numString) => Number(numString));
+const processInput = (filename: string): Record<string, number> => {
+  return countBy(readLines(filename)[0].split(" "));
+};
+
+const addToCount = (
+  stoneCounts: Record<string, number>,
+  stone: string,
+  count: number
+): void => {
+  if (!(stone in stoneCounts)) {
+    stoneCounts[stone] = 0;
+  }
+  stoneCounts[stone] += count;
 };
 
 const executeBlinks = (filename: string, numBlinks: number): number => {
-  let stones = processInput(filename);
+  let stoneCounts = processInput(filename);
 
   for (let i = 0; i < numBlinks; i++) {
-    const newStones = [];
+    const nextStoneCounts: Record<string, number> = {};
 
-    for (const stone of stones) {
-      if (stone === 0) {
-        newStones.push(1);
+    for (const [stone, count] of Object.entries(stoneCounts)) {
+      if (stone === "0") {
+        addToCount(nextStoneCounts, "1", count);
         continue;
       }
 
-      const stoneAsString = stone.toString();
-      const stoneDigits = stoneAsString.length;
+      const stoneDigits = stone.length;
       if (stoneDigits % 2 === 0) {
-        newStones.push(Number(stoneAsString.slice(0, stoneDigits / 2)));
-        newStones.push(Number(stoneAsString.slice(stoneDigits / 2)));
+        const left = stone.slice(0, stoneDigits / 2);
+        const right = stone.slice(stoneDigits / 2);
+
+        addToCount(nextStoneCounts, Number(left).toString(), count);
+        addToCount(nextStoneCounts, Number(right).toString(), count);
         continue;
       }
 
-      newStones.push(stone * 2024);
+      addToCount(nextStoneCounts, (Number(stone) * 2024).toString(), count);
     }
 
-    stones = newStones;
+    stoneCounts = nextStoneCounts;
   }
 
-  return stones.length;
+  return sum(Object.values(stoneCounts));
 };
 
 // Part 1 test
@@ -42,3 +54,7 @@ console.log(executeBlinks("src/day11/test-input.txt", 25));
 // Part 1
 console.log(executeBlinks("src/day11/input.txt", 25));
 // 218079
+
+// Part 2
+console.log(executeBlinks("src/day11/input.txt", 75));
+// 259755538429618
